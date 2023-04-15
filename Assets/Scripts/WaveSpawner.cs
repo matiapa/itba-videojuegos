b0d76d58fe625c425fea1aa085eae69f71a9977e
Matias J. Apablaza <mapablaza@itba.edu.ar>
2023-04-14 21:59:08 -0300

    Fixed issue with WaveSpawner not waiting the appropiate time between waves
    Former-commit-id: f2eebc855a420fd9e0959897412fe1f327deb252 [formerly e8e556321627afc1db9af1682e7dbe34b52b45d2]
    Former-commit-id: 62e6f8ae813257dfe0ccac1198b07e08af1b52c9

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -2,37 +2,36 @@ using System.Collections;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour {
+
     public Wave[] waves;
     public Transform spawnPoint;
     public float timeBetweenWaves = 60f;
+    
     private float countdown = 2f;
     private int waveIndex = 0;
 
-    void Update ()
-    {
+    void Update () {
         if (waveIndex == waves.Length)
-        {
             this.enabled = false;
-        }
 
-        if (countdown <= 0f)
-        {
+        if (countdown <= 0f) {
             StartCoroutine(SpawnWave());
-            countdown = timeBetweenWaves;
+            countdown = timeBetweenWaves + waves[waveIndex].Duration;
             return;
         }
 
         countdown -= Time.deltaTime;
     }
     
-    IEnumerator SpawnWave ()
-    {
+    IEnumerator SpawnWave () {
         Wave wave = waves[waveIndex];
-        for (int i = 0; i < wave.count; i++)
-        {
+        
+        for (int i = 0; i < wave.count; i++) {
             Instantiate(wave.enemy, spawnPoint.position, spawnPoint.rotation);
             yield return new WaitForSeconds(1f / wave.rate);
         }
+        
         waveIndex++;
     }
+
 }
