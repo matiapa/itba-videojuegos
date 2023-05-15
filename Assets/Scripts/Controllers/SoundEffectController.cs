3df0da50ac58d1c44322bb1a4c96b9af76fe9c45
Santiago Sandrini <ssandrini@itba.edu.ar>
2023-05-15 10:23:02 -0300

    SoundEffectController
    Former-commit-id: 264888406ec24579235e0630c98c729d5dbdf619 [formerly 23c40f0e1b1d2b5f396c44d93140f6cbf09a403d]
    Former-commit-id: f8866bab98e773334da3fecb27e1c875ffeadb10

---
+++ Assets/Scripts/Controllers/SoundEffectController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+    
+public class SoundEffectController : MonoBehaviour
+{
+    [SerializeField] private AudioClip _victoryAudioClip;
+    [SerializeField] private AudioClip _defeatAudioClip;
+
+    public AudioSource AudioSource => _audioSource;
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        EventManager.instance.OnGameOver += OnGameOver;
+    }
+
+    private void OnGameOver(bool isVictory)
+    {
+        _audioSource.PlayOneShot(isVictory? _victoryAudioClip : _defeatAudioClip);
+    }
+}
